f8b495abfbdda19f74e58b33345eaeb58b492664
PhilMinecraft <arturo.rabino.3@outlook.com>
2022-07-26 18:17:34 +0800

    .

--- script.js
+++ script.js
@@ -205,7 +205,11 @@ function readMoreLess2() {
 
 
 
-window.onresize = function() {
-  document.body.height = window.innerHeight;
+function resetHeight(){
+  // reset the body height to that of the inner browser
+  document.body.style.height = window.innerHeight + "px";
 }
-window.onresize();
+// reset the height whenever the window's resized
+window.addEventListener("resize", resetHeight);
+// called to initially set the height.
+resetHeight();
